1e421d5c4b701215b17b61c1fdb0403f690132ba
Haider Ali <haider.ali101@hotmail.com>
2019-07-25 01:17:48 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Another pull request for testing :)
+          Another pull request for testing Heroku part 3:)
         </p>
         <a
           className="App-link"
