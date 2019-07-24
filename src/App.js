bbf57da41acc13c0031fd926c4542f3c7015cfed
Haider Ali <haider.ali101@hotmail.com>
2019-07-25 01:06:50 +0500

    Updated Link

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Access Google
         </a>
       </header>
     </div>
